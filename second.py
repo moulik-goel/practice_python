57e36df1c01c5ddae9b93a6317af3191a60d1e62
moulikgoel <163639651+moulikgoel@users.noreply.github.com>
2024-07-19 10:14:34 +0530

    variable file uploaded

--- second.py
+++ second.py
@@ -1,3 +1,26 @@
 name = "second day"
 
 print(name)
+
+a = 22
+
+a = str(6)
+
+b = "hi this is 3rd class"
+
+#type cast of int to str
+c = 12
+
+x = a+b
+
+sum = int(a) + int(c)
+
+print(type(a))
+print(type(name))
+
+print(x)
+
+print(a)
+
+print(sum)
+
---
+++ variable.py
@@ -0,0 +1,13 @@
+name="moulik goel"
+age=20
+degree="biotechnology"
+
+print(name,age,degree)
+
+print('hi, '+ name + 'your age is  '+ str(age) + 'and you are pursuing '+ degree)
+
+#print(`Hi, ${name} your age is ${age} and you are pursuing ${degree}`)
+      
+print(f"Hi, {name} your age is {age} and you are pursuing {degree}")
+
+print(f"hi, {name}, your age is {age} and you are persuing {degree}")
